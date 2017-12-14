0e000008a1594caf1a321f4591fb1815b233f59a
BazzaLuca <lucabazza123@gmail.com>
2017-12-14 11:53:53 +0100

    modifica home con immagine

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ app.get('/', function(req, res) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(data);
         }
+        console.log(arrayTopics);
 	);
 });
 
